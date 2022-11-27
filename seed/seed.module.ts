f2d7be59b0c683b6dfa42b7cfe9c80e3b983862f
ihor <ihor.bielchenko@gmail.com>
2022-11-27 15:51:11 +0100

    Update seeders.

--- seed/seed.module.ts
+++ seed/seed.module.ts
@@ -5,25 +5,17 @@ import { CacheService } from '@nest-datum/services';
 import { typeormConfig } from 'config/typeorm';
 import { redisConfig } from 'config/redis';
 import { SeedService } from './seed.service';
-import { TemplateStatus } from 'src/api/template-status/template-status.entity';
-import { TemplateOption } from 'src/api/template-option/template-option.entity';
-import { TemplateTemplateOption } from 'src/api/template-template-option/template-template-option.entity';
-import { TemplateTemplateTemplateOption } from 'src/api/template-template-template-option/template-template-template-option.entity';
-import { Template } from 'src/api/template/template.entity';
-import { LetterStatus } from 'src/api/letter-status/letter-status.entity';
-import { LetterOption } from 'src/api/letter-option/letter-option.entity';
-import { LetterLetterOption } from 'src/api/letter-letter-option/letter-letter-option.entity';
-import { LetterLetterLetterOption } from 'src/api/letter-letter-letter-option/letter-letter-letter-option.entity';
-import { Letter } from 'src/api/letter/letter.entity';
-import { Report } from 'src/api/report/report.entity';
+import { TypeStatus } from 'src/api/type-status/type-status.entity';
+import { TypeOption } from 'src/api/type-option/type-option.entity';
+import { TypeTypeOption } from 'src/api/type-type-option/type-type-option.entity';
+import { TypeTypeTypeOption } from 'src/api/type-type-type-option/type-type-type-option.entity';
+import { Type } from 'src/api/type/type.entity';
 import { Setting } from 'src/api/setting/setting.entity';
-import { TemplateStatusSeeder } from './template-status.seeder';
-import { TemplateOptionSeeder } from './template-option.seeder';
-import { TemplateTemplateOptionSeeder } from './template-template-option.seeder';
-import { TemplateTemplateTemplateOptionSeeder } from './template-template-template-option.seeder';
-import { TemplateSeeder } from './template.seeder';
-import { LetterStatusSeeder } from './letter-status.seeder';
-import { LetterSeeder } from './letter.seeder';
+import { TypeStatusSeeder } from './type-status.seeder';
+import { TypeOptionSeeder } from './type-option.seeder';
+import { TypeTypeOptionSeeder } from './type-type-option.seeder';
+import { TypeTypeTypeOptionSeeder } from './type-type-type-option.seeder';
+import { TypeSeeder } from './type.seeder';
 import { SettingSeeder } from './setting.seeder';
 
 @Module({
@@ -32,30 +24,22 @@ import { SettingSeeder } from './setting.seeder';
 		TypeOrmModule.forRoot(typeormConfig),
 		RedisModule.forRoot(redisConfig),
 		TypeOrmModule.forFeature([
-			TemplateStatus,
-			TemplateOption,
-			TemplateTemplateOption,
-			TemplateTemplateTemplateOption,
-			Template,
-			LetterStatus,
-			LetterOption,
-			LetterLetterOption,
-			LetterLetterLetterOption,
-			Letter,
-			Report,
+			TypeStatus,
+			TypeOption,
+			TypeTypeOption,
+			TypeTypeTypeOption,
+			Type,
 			Setting,
 		]),
 	],
 	providers: [
 		CacheService,
 		SeedService,
-		TemplateStatusSeeder,
-		TemplateOptionSeeder,
-		TemplateTemplateOptionSeeder,
-		TemplateTemplateTemplateOptionSeeder,
-		TemplateSeeder,
-		LetterStatusSeeder,
-		LetterSeeder,
+		TypeStatusSeeder,
+		TypeOptionSeeder,
+		TypeTypeOptionSeeder,
+		TypeTypeTypeOptionSeeder,
+		TypeSeeder,
 		SettingSeeder,
 	]
 })
