347ce6ccd24127026a6c2656107d7246ec84a51f
ihor <ihor.bielchenko@gmail.com>
2023-02-06 08:28:53 +0100

    test

--- src/api/type-option/type-option.service.ts
+++ src/api/type-option/type-option.service.ts
@@ -6,18 +6,21 @@ import {
 } from 'typeorm';
 import { OptionService as NestDatumOptionService } from '@nest-datum/option';
 import { CacheService } from '@nest-datum/cache';
+import { TypeTypeOption } from '../type-type-option/type-type-option.entity';
 import { TypeOption } from './type-option.entity';
 
 @Injectable()
 export class TypeOptionService extends NestDatumOptionService {
 	public entityName = 'typeOption';
+	public entityColumnOption = 'typeOptionId';
 	public entityConstructor = TypeOption;
 
 	constructor(
 		@InjectRepository(TypeOption) public repository: Repository<TypeOption>,
+		@InjectRepository(TypeTypeOption) public repositoryOptionOption: Repository<TypeTypeOption>,
 		public connection: Connection,
 		public cacheService: CacheService,
 	) {
-		super(repository, connection, cacheService);
+		super(repository, repositoryOptionOption, connection, cacheService);
 	}
 }
