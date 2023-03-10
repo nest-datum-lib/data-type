735e79701fb7eaafb10532fdb2b2925543b2e50d
ihor <ihor.bielchenko@gmail.com>
2023-03-10 12:23:14 +0100

    test

--- src/api/type/type.service.ts
+++ src/api/type/type.service.ts
@@ -54,9 +54,9 @@ export class TypeService extends OptionEntityService {
 		if (utilsCheckObj(filter['custom'])) {
 			const optionId = filter['custom']['disableTypeForOption'];
 			const queryRunner = await this.connection.createQueryRunner();
-			const types = await queryRunner.query(`SELECT id, typeId FROM type_type_option WHERE typeOptionId = '${optionId}'`);
+			const types = await queryRunner.query(`SELECT id, typeId, typeOptionId FROM type_type_option WHERE typeOptionId = '${optionId}' GROUP BY typeId`);
 
-			console.log('types', `SELECT id, typeId FROM type_type_option WHERE typeOptionId = '${optionId}'`, optionId, types);
+			console.log('types', `SELECT id, typeId, typeOptionId FROM type_type_option WHERE typeOptionId = '${optionId}' GROUP BY typeId`, optionId, types);
 
 			delete filter['custom'];
 		}
