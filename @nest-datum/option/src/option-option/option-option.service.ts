4c39aeb630f38c465b371aaa2fc57af09b1dcaf9
ihor <ihor.bielchenko@gmail.com>
2023-03-15 20:22:23 +0100

    test

--- @nest-datum/option/src/option-option/option-option.service.ts
+++ @nest-datum/option/src/option-option/option-option.service.ts
@@ -9,6 +9,12 @@ export class OptionOptionService extends SqlService {
 	protected entityOptionName;
 
 	protected manyGetColumns(customColumns: object = {}) {
+		console.log('>>>>>>>>>>', ({
+			...super.manyGetColumns(),
+			[this.entityId]: true,
+			[this.entityOptionId]: true,
+		}));
+
 		return ({
 			...super.manyGetColumns(),
 			[this.entityId]: true,
