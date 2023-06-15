dbb0406e0bbd0b9aad9354d345f58ab6a0adfd60
ihor <ihor.bielchenko@gmail.com>
2023-06-15 11:51:56 +0200

    Test seeds.

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -25,7 +25,7 @@ export class SeedService {
 	) {
 		this.seeders = [
 			this.settings,
-			// this.typeStatus,
+			this.typeStatus,
 			// this.typeOption,
 			// this.type,
 			// this.typeTypeOption,
